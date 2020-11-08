5bafb7ba211e296e0bf834d2ad3e24bb5263f346
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2020-11-07 20:44:53 -0500

    Puzzle Data
    Loaded a single elimination grid to firebase and render all of the data on the screen. Still needs to be formatted and applied as a game

--- src/screens/DataTesting.js
+++ src/screens/DataTesting.js
@@ -6,58 +6,80 @@ import * as firebase from 'firebase';
 
 class DataTesting extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        
-      }
-      this.ref = firebase.firestore().collection('users');
-    }
-  
-    async componentDidMount() {
-  
-        this.unsubscribe = this.ref.onSnapshot(async (querySnapshot) => {
-            var userArray = [];
-            querySnapshot.forEach((doc) => {
-                userArray.push({
-                age: doc.data().age,
-                email: doc.data().email,
-                name: doc.data().name,
-              })
-            })
-            console.log(userArray);
-            this.setState({
-              users: userArray,
-              age: userArray[0].age,
-              email: userArray[0].email,
-              name: userArray[0].name
+  constructor(props) {
+    super(props);
+    this.state = {
 
-            })
-      
-            this.unsubscribe();
-          })
     }
-  
-   
-  
-  
-    render() {
-        return (
-            <div>
-              <h2>Data Testing Screen</h2>
-              <p>{this.state.age}</p>
-              <p>{this.state.email}</p>
-              <p>{this.state.name}</p>
-
-            </div>
-          );
-    }
-  
+    this.ref = firebase.firestore().collection('eliminationgrids');
   }
-  
-  
 
-  export default DataTesting;
+  async componentDidMount() {
+
+    this.unsubscribe = this.ref.onSnapshot(async (querySnapshot) => {
+      var puzzleArray = [];
+      querySnapshot.forEach((doc) => {
+        puzzleArray.push({
+          cat1: doc.data().cat1,
+          cat1op: doc.data().cat1options,
+          cat2: doc.data().cat2,
+          cat2op: doc.data().cat2options,
+          cat3: doc.data().cat3,
+          cat3op: doc.data().cat3options,
+          cat4: doc.data().cat4,
+          cat4op: doc.data().cat4options,
+          solution: doc.data().solution,
+          size: doc.data().size,
+        })
+      })
+      console.log(puzzleArray);
+
+      var mapToArray = [];
+      for (let i = 1; i < puzzleArray[0].size; i++) {
+        mapToArray.push(puzzleArray[0].solution[i] + " ");
+      }
+      console.log(mapToArray);
+
+      this.setState({
+        puzzle: puzzleArray,
+        solution: mapToArray,
+
+      })
+
+      this.unsubscribe();
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h2>Data Testing Screen</h2>
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat1}</p>}
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat1op}</p>}
+
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat2}</p>}
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat2op}</p>}
+
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat3}</p>}
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat3op}</p>}
+
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat4}</p>}
+        {this.state.puzzle != undefined && <p>{this.state.puzzle[0].cat4op}</p>}
+
+        {this.state.puzzle != undefined && <p>{this.state.solution}</p>}
+
+      </div>
+    );
+  }
+
+}
+
+
+
+export default DataTesting;
 
 
 
@@ -66,6 +88,6 @@ class DataTesting extends React.Component {
 
 
 //Leave this here. May need later
- {/* <Link routeName="Profile" params={{ name: "brent" }}>
+{/* <Link routeName="Profile" params={{ name: "brent" }}>
                 Go to Brent's Profile
               </Link> */}
