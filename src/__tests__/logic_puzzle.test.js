0dfcc51521241d336faf3b6dab5e47765e5a5279
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2020-11-27 12:53:13 -0500

    Unit Testing
    Implemented preliminary unit tests to verify the output of the puzzle chekcer function

--- src/__tests__/logic_puzzle.test.js
+++ src/__tests__/logic_puzzle.test.js
@@ -1,14 +1,112 @@
-describe("Filter function", () => {
-    test("it should filter by a search term (link)", () => {
-      const input = [
-        { id: 1, url: "https://www.url1.dev" },
-        { id: 2, url: "https://www.url2.dev" },
-        { id: 3, url: "https://www.link3.dev" }
-      ];
-  
-      const output = [{ id: 3, url: "https://www.link3.dev" }];
-  
-      expect(filterByTerm(input, "link")).toEqual(output);
-  
-    });
+describe("Check puzzle function", () => {
+  test("It should return a score for full points", () => {
+    const userSolution = [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+    ]
+
+    const solution = [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+    ]
+
+
+
+    let score = 0;
+    for (let x = 0; x < userSolution.length; x++) {
+      for (let y = 0; y < userSolution[0].length; y++) {
+        if (userSolution[x][y] != solution[x][y]) {
+          console.log("Your puzzle is wrong");
+          score--;
+        }
+        score++;
+      }
+    }
+
+
+    expect(score).toEqual(36);
+
   });
+});
+
+
+
+describe("Check puzzle function", () => {
+  test("It should return a score of 0", () => {
+    const userSolution = [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+    ]
+
+    const solution = []
+
+
+
+    let score = 0;
+    if(solution.length != 0) {
+
+      for (let x = 0; x < userSolution.length; x++) {
+        for (let y = 0; y < userSolution[0].length; y++) {
+          if (userSolution[x][y] != solution[x][y]) {
+            console.log("Your puzzle is wrong");
+            score--;
+          }
+          score++;
+        }
+      }
+    } 
+
+    expect(score).toEqual(0);
+
+  });
+});
+
+
+
+describe("Check puzzle function", () => {
+  test("It should return a score of 18", () => {
+    const userSolution = [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+    ]
+
+    const solution = [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    ]
+
+
+
+    let score = 0;
+    if(solution.length != 0) {
+
+      for (let x = 0; x < userSolution.length; x++) {
+        for (let y = 0; y < userSolution[0].length; y++) {
+          if (userSolution[x][y] != solution[x][y]) {
+            console.log("Your puzzle is wrong");
+            score--;
+          }
+          score++;
+        }
+      }
+    } 
+    
+    expect(score).toEqual(18);
+
+  });
+});
